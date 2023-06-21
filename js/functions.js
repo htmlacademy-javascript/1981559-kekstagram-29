c377b4392852809b7f6713bff47ae681f58998c4
Vladislav Gernet <skeimorr@gmail.com>
2023-06-21 12:40:50 +0400

    lint check done

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,36 @@
 //Функция для проверки длины строки.
 
-const checkLength = (string, maxLength) => {
-  return string.length <= maxLength;
-}
+const checkLength = (string, maxLength) => string.length <= maxLength;
+
+checkLength();
 
 //Тесты для функции для проверки длины строки.
-console.log('Тесты для функции для проверки длины строки.');
-console.log(checkLength('проверяемая строка', 20));
-console.log(checkLength('проверяемая строка', 18));
-console.log(checkLength('проверяемая строка', 10));
+// console.log('Тесты для функции для проверки длины строки.');
+// console.log(checkLength('проверяемая строка', 20));
+// console.log(checkLength('проверяемая строка', 18));
+// console.log(checkLength('проверяемая строка', 10));
 
 //Функция проверки на палиндром
 
 const checkPalindrome = (string) => {
-  let formattedString = string.replaceAll(' ', '').toLowerCase();
+  const formattedString = string.replaceAll(' ', '').toLowerCase();
   let newString = '';
-  for ( let i = formattedString.length - 1; i >= 0; i-- ) {
+  for (let i = formattedString.length - 1; i >= 0; i--) {
     newString += formattedString.at(i);
   }
 
   return formattedString === newString;
-}
+};
 
-console.log('Тесты для функции проверки на палиндром.');
-console.log(checkPalindrome('топот'));
-console.log(checkPalindrome('ДовОд'));
-console.log(checkPalindrome('Кекс'));
-console.log('Палиндром с пробелами.')
-console.log(checkPalindrome('Лёша на полке клопа нашёл '));
+checkPalindrome();
+
+// Тесты для функции проверки на палиндром.
+// console.log('Тесты для функции проверки на палиндром.');
+// console.log(checkPalindrome('топот'));
+// console.log(checkPalindrome('ДовОд'));
+// console.log(checkPalindrome('Кекс'));
+// console.log('Палиндром с пробелами.')
+// console.log(checkPalindrome('Лёша на полке клопа нашёл '));
 
 //Функция поиска цифр в строке.
 
@@ -37,23 +40,30 @@ const findNumbers = (string) => {
   let newValue = '';
 
   for (let i = 0; i <= newString.length; i++) {
-    let isNumber = +(newString.at(i));
-    if (isNaN(isNumber)) continue;
+    const isNumber = +(newString.at(i));
+    if (isNaN(isNumber)) {
+      continue;
+    }
     newValue += isNumber;
   }
 
-  if (newValue === '') return NaN;
+  if (newValue === '') {
+    return NaN;
+  }
 
   return +newValue;
-}
+};
 
-console.log('Тесты дополниетельного задания.')
-console.log(findNumbers('2023 год'));
-console.log(findNumbers('ECMAScript 2022'));
-console.log(findNumbers('1 кефир, 0.5 батона'));
-console.log(findNumbers('агент 007'));
-console.log(findNumbers('а я томат'));
-console.log('Усложненное задание.')
-console.log(findNumbers(2023));
-console.log(findNumbers(-1));
-console.log(findNumbers(1.5));
+findNumbers();
+
+// Тесты дополниетельного задания.
+// console.log('Тесты дополниетельного задания.')
+// console.log(findNumbers('2023 год'));
+// console.log(findNumbers('ECMAScript 2022'));
+// console.log(findNumbers('1 кефир, 0.5 батона'));
+// console.log(findNumbers('агент 007'));
+// console.log(findNumbers('а я томат'));
+// console.log('Усложненное задание.')
+// console.log(findNumbers(2023));
+// console.log(findNumbers(-1));
+// console.log(findNumbers(1.5));
