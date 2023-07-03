2649db94eac9abf494e2e09b72983808f4398340
Vladislav Gernet <skeimorr@gmail.com>
2023-07-03 12:27:23 +0400

    comment structure

--- js/fullphoto-rendering.js
+++ js/fullphoto-rendering.js
@@ -34,13 +34,34 @@ const onCardClick = (evt) => {
   const bigPictureLikes = bigPicture.querySelector('.likes-count');
   const bigPictureDescription = bigPicture.querySelector('.social__caption');
   const bigPictureCommentsValue = bigPicture.querySelector('.comments-count');
+  const bigPictureCommentsList = bigPicture.querySelector('.social__comments');
+  const bigPictureCommentsItem = bigPicture.querySelectorAll('.social__comment');
 
   if (evt.target.matches('.picture__img')) {
-    let selectedPictureId = evt.target.dataset.pictureId;
-    bigPictureImage.src = `./photos/${selectedPictureId}.jpg`
+    const selectedPictureId = evt.target.dataset.pictureId;
+    bigPictureImage.src = `./photos/${selectedPictureId}.jpg`;
     bigPictureLikes.textContent = String(newArrayOfObjects[selectedPictureId - 1].likes);
     bigPictureDescription.textContent = newArrayOfObjects[selectedPictureId - 1].description;
     bigPictureCommentsValue.textContent = String(newArrayOfObjects[selectedPictureId - 1].comments.length);
+    bigPictureCommentsItem.forEach(item => item.remove());
+
+    const newElement = document.createElement('li');
+    newElement.classList.add('social__comment');
+
+    const newAvatar = document.createElement('img');
+    newAvatar.classList.add('social__picture');
+    newAvatar.src = './img/avatar-1.svg';
+    newAvatar.alt = 'Имя комметатора';
+    newAvatar.width = 35;
+    newAvatar.height = 35;
+    newElement.appendChild(newAvatar);
+
+    const newCommentContent = document.createElement('p');
+    newCommentContent.classList.add('social__text');
+    newCommentContent.textContent = 'Текст комментария';
+    newElement.appendChild(newCommentContent);
+
+    bigPictureCommentsList.appendChild(newElement)
   }
 };
 
