7de5ca10e98038eb9cbd5322073b7a5940506241
Vladislav Gernet <skeimorr@gmail.com>
2023-07-08 18:48:18 +0400

    new solution to show comments

--- js/full-photo-click-handler.js
+++ js/full-photo-click-handler.js
@@ -1,6 +1,7 @@
 import {isEscapeKey} from './util.js';
-import {createFullPhotoCard, addCommentsInFullPhotoCard} from './full-photo-creator.js';
-import {createCommentList} from './full-photo-comments-creator.js';
+import {newArrayOfObjects, DEFAULT_SHOWN_COMMENTS} from './data.js';
+import {createFullPhotoCard} from './full-photo-creator.js';
+import {addCommentsInFullPhotoCard} from './full-photo-comments-creator.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
@@ -8,6 +9,8 @@ const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
 const bigPictureLikes = bigPicture.querySelector('.likes-count');
 const bigPictureDescription = bigPicture.querySelector('.social__caption');
 const bigPictureCommentsValue = bigPicture.querySelector('.comments-count');
+const bigPictureCommentsList = bigPicture.querySelector('.social__comments');
+const showMoreButton = bigPicture.querySelector('.comments-loader');
 const cardPictureWall = document.querySelector('.pictures');
 
 const onEscapeClick = (evt) => {
@@ -29,18 +32,27 @@ const onPictureClick = () => {
   document.addEventListener('keydown', onEscapeClick);
 };
 
-const onCardClick = (evt) => {
+const onCardClickCreate = (evt) => {
   if (evt.target.closest('.picture')) {
     document.body.classList.add('modal-open');
     const selectedPictureId = evt.target.closest('.picture').dataset.pictureId;
-    createFullPhotoCard(bigPicture, bigPictureImage, bigPictureLikes, bigPictureDescription, bigPictureCommentsValue, selectedPictureId);
+    // const messagesArrayLength = newArrayOfObjects[selectedPictureId - 1].comments.length;
 
-    const newCommentsInCard = addCommentsInFullPhotoCard(selectedPictureId);
+    createFullPhotoCard(bigPictureImage, bigPictureLikes, bigPictureDescription, bigPictureCommentsValue, selectedPictureId);
+    // Здесь можно попробовать объект сосздать, чтобы столько данных не писать.
 
-    createCommentList(selectedPictureId, newCommentsInCard);
+    addCommentsInFullPhotoCard(DEFAULT_SHOWN_COMMENTS, selectedPictureId);
+    let initialSownCardsValue = DEFAULT_SHOWN_COMMENTS;
+
+    showMoreButton.addEventListener('click', () => {
+      initialSownCardsValue += DEFAULT_SHOWN_COMMENTS;
+      addCommentsInFullPhotoCard(initialSownCardsValue, selectedPictureId);
+    });
+
+    // hideCommentsLoader(messagesArrayLength)
   }
 };
 
-cardPictureWall.addEventListener('click', onCardClick);
+cardPictureWall.addEventListener('click', onCardClickCreate);
 
-export {onPictureClick};
+export {onPictureClick, bigPictureCommentsList, showMoreButton};
--- js/full-photo-comments-creator.js
+++ js/full-photo-comments-creator.js
@@ -1,55 +1,109 @@
-import {newArrayOfObjects} from './data.js';
+import {newArrayOfObjects, DEFAULT_SHOWN_COMMENTS} from './data.js';
+import {bigPictureCommentsList, showMoreButton} from './full-photo-click-handler.js';
 
-const DEFAULT_SHOWN_COMMENTS = 5;
-const commentsList = document.querySelector('.social__comments');
-const bigPictureCommentsCount = document.querySelector('.social__comment-count');
-const bigPictureCommentsLoader = document.querySelector('.comments-loader');
-
-
-const createCommentList = (pictureId, commentsArray) => {
+const addCommentsInFullPhotoCard = (valueOfShownComments, pictureId) => {
   const messagesArray = newArrayOfObjects[pictureId - 1].comments;
-  if (messagesArray.length) {
-    commentsList.appendChild(commentsArray);
-  }
+  bigPictureCommentsList.innerHTML = '';
 
-  const showMoreComments = () => {
-    const hiddenComments = Array.from(commentsList.querySelectorAll('.social__comment.hidden'));
-    const commentCounterContainer = document.querySelector('.social__comment-count');
-    const commentCounterContainerValue = commentCounterContainer.innerHTML.split(' ')[0];
+  const messagesFragment = document.createDocumentFragment();
 
-    if (hiddenComments.length >= DEFAULT_SHOWN_COMMENTS) {
-      for (let i = 0; i < DEFAULT_SHOWN_COMMENTS; i++) {
-        hiddenComments[i].classList.remove('hidden');
-      }
-      const shownCommentsValue = Number(commentCounterContainerValue) + Number(DEFAULT_SHOWN_COMMENTS);
-      commentCounterContainer.innerHTML = `${shownCommentsValue} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
-    } else if (hiddenComments.length < DEFAULT_SHOWN_COMMENTS) {
-      for (let i = 0; i < hiddenComments.length; i++) {
-        hiddenComments[i].classList.remove('hidden');
-      }
-      const shownCommentsValue = Number(commentCounterContainerValue) + Number(hiddenComments.length);
-      commentCounterContainer.innerHTML = `${shownCommentsValue} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
+  messagesArray.some((comment, index) => {
+    if (index + 1 > valueOfShownComments) {
+      return true;
     }
 
-    if (hiddenComments.length <= DEFAULT_SHOWN_COMMENTS) {
-      bigPictureCommentsLoader.classList.add('hidden');
-    }
-  };
+    const newElement = document.createElement('li');
+    newElement.classList.add('social__comment');
 
-  bigPictureCommentsLoader.classList.add('hidden');
-  if (!messagesArray.length) {
-    bigPictureCommentsCount.textContent = '0 комментариев';
-  } else if (messagesArray.length === 1) {
-    bigPictureCommentsCount.textContent = '1 комментарий';
-  } else if (messagesArray.length > 1 && messagesArray.length < 5) {
-    bigPictureCommentsCount.textContent = `${messagesArray.length} комментария`;
-  } else if (messagesArray.length === DEFAULT_SHOWN_COMMENTS) {
-    bigPictureCommentsCount.textContent = `${DEFAULT_SHOWN_COMMENTS} комментариев`;
-  } else {
-    bigPictureCommentsCount.innerHTML = `${DEFAULT_SHOWN_COMMENTS} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
-    bigPictureCommentsLoader.classList.remove('hidden');
-    bigPictureCommentsLoader.onclick = showMoreComments;
-  }
+    const newAvatar = document.createElement('img');
+    newAvatar.classList.add('social__picture');
+    newAvatar.src = comment.avatar;
+    newAvatar.alt = comment.name;
+    newAvatar.width = 35;
+    newAvatar.height = 35;
+    newElement.appendChild(newAvatar);
+
+    const newCommentContent = document.createElement('p');
+    newCommentContent.classList.add('social__text');
+    newCommentContent.textContent = comment.message;
+    newElement.appendChild(newCommentContent);
+
+    messagesFragment.appendChild(newElement);
+    return false;
+  });
+
+  bigPictureCommentsList.appendChild(messagesFragment);
 };
 
-export {createCommentList};
+// const hideCommentsLoader = (valueOfComments) => {
+//
+//   if (DEFAULT_SHOWN_COMMENTS >= valueOfComments) {
+//     showMoreButton.classList.add('hidden');
+//   } else {
+//     showMoreButton.classList.remove('hidden')
+//   }
+//
+//   // if (!messagesArray.length) {
+//   //   bigPictureCommentsCount.textContent = '0 комментариев';
+//   // } else if (messagesArray.length === 1) {
+//   //   bigPictureCommentsCount.textContent = '1 комментарий';
+//   // } else if (messagesArray.length > 1 && messagesArray.length < 5) {
+//   //   bigPictureCommentsCount.textContent = `${messagesArray.length} комментария`;
+//   // } else if (messagesArray.length === DEFAULT_SHOWN_COMMENTS) {
+//   //   bigPictureCommentsCount.textContent = `${DEFAULT_SHOWN_COMMENTS} комментариев`;
+//   // } else {
+//   //   bigPictureCommentsCount.innerHTML = `${DEFAULT_SHOWN_COMMENTS} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
+//   //   bigPictureCommentsLoader.classList.remove('hidden');
+//   // }
+// }
+
+
+//
+//
+// const createCommentList = (pictureId, commentsArray) => {
+//   const messagesArray = newArrayOfObjects[pictureId - 1].comments;
+//   if (messagesArray.length) {
+//     commentsList.appendChild(commentsArray);
+//   }
+
+  // const showMoreComments = () => {
+  //   const hiddenComments = Array.from(commentsList.querySelectorAll('.social__comment.hidden'));
+  //   const commentCounterContainer = document.querySelector('.social__comment-count');
+  //   const commentCounterContainerValue = commentCounterContainer.innerHTML.split(' ')[0];
+  //
+  //   if (hiddenComments.length >= DEFAULT_SHOWN_COMMENTS) {
+  //     for (let i = 0; i < DEFAULT_SHOWN_COMMENTS; i++) {
+  //       hiddenComments[i].classList.remove('hidden');
+  //     }
+  //     const shownCommentsValue = Number(commentCounterContainerValue) + Number(DEFAULT_SHOWN_COMMENTS);
+  //     commentCounterContainer.innerHTML = `${shownCommentsValue} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
+  //   } else if (hiddenComments.length < DEFAULT_SHOWN_COMMENTS) {
+  //     for (let i = 0; i < hiddenComments.length; i++) {
+  //       hiddenComments[i].classList.remove('hidden');
+  //     }
+  //     const shownCommentsValue = Number(commentCounterContainerValue) + Number(hiddenComments.length);
+  //     commentCounterContainer.innerHTML = `${shownCommentsValue} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
+  //   }
+  //
+  //   if (hiddenComments.length <= DEFAULT_SHOWN_COMMENTS) {
+  //     bigPictureCommentsLoader.classList.add('hidden');
+  //   }
+  // };
+
+  // bigPictureCommentsLoader.classList.add('hidden');
+  // if (!messagesArray.length) {
+  //   bigPictureCommentsCount.textContent = '0 комментариев';
+  // } else if (messagesArray.length === 1) {
+  //   bigPictureCommentsCount.textContent = '1 комментарий';
+  // } else if (messagesArray.length > 1 && messagesArray.length < 5) {
+  //   bigPictureCommentsCount.textContent = `${messagesArray.length} комментария`;
+  // } else if (messagesArray.length === DEFAULT_SHOWN_COMMENTS) {
+  //   bigPictureCommentsCount.textContent = `${DEFAULT_SHOWN_COMMENTS} комментариев`;
+  // } else {
+  //   bigPictureCommentsCount.innerHTML = `${DEFAULT_SHOWN_COMMENTS} из <span class="comments-count">${String(messagesArray.length)}</span> комментариев`;
+  //   bigPictureCommentsLoader.classList.remove('hidden');
+    // bigPictureCommentsLoader.onclick = showMoreComments;
+//   }
+// };
+
+export {addCommentsInFullPhotoCard};
