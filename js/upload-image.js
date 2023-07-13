57942bd0946b5229077cc27221d8ef543246f7e2
Vladislav Gernet <skeimorr@gmail.com>
2023-07-13 20:22:26 +0400

    new simple method

--- js/upload-image.js
+++ js/upload-image.js
@@ -1,10 +1,38 @@
-import {createClosePictureHandle} from "./util.js";
+import {isEscapeKey} from "./util.js";
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadImage = uploadForm.querySelector('.img-upload__input');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const cancelUploadButton = uploadForm.querySelector('.img-upload__cancel');
 
-const cancelUpload = createClosePictureHandle(uploadOverlay, cancelUploadButton);
+const doAfterClose = () => {
+  document.removeEventListener('keydown', onEscapeClick);
+  cancelUploadButton.removeEventListener('click', doAfterClose);
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+}
 
-uploadImage.addEventListener('click', cancelUpload);
+const onEscapeClick = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    doAfterClose();
+  }
+};
+
+const onUploadButtonClick = () => {
+  document.body.classList.add('modal-open');
+  uploadOverlay.classList.remove('hidden');
+  cancelUploadButton.addEventListener('click', doAfterClose);
+  document.addEventListener('keydown', onEscapeClick);
+}
+
+const openUploadOverlay = () => {
+  onUploadButtonClick();
+}
+
+uploadImage.addEventListener('change', openUploadOverlay);
+
+/*
+Остановился на 5 пункте выполнения задания
+Возможно можно узнать в нейросети
+ */
