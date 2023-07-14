16024f1e9e715adb09bdb7c5528dafbba3fe32db
Vladislav Gernet <skeimorr@gmail.com>
2023-07-14 13:37:29 +0400

    check hashtag done

--- js/upload-image.js
+++ js/upload-image.js
@@ -13,6 +13,7 @@ const doAfterClose = () => {
   uploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   uploadForm.reset();
+  pristine.reset();
 }
 
 const onEscapeClick = (evt) => {
@@ -34,20 +35,36 @@ uploadImage.addEventListener('change', onUploadButtonClick);
 isFocusedElement(hashTagInput);
 isFocusedElement(commentField);
 
+const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 
-// const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-// // console.log(hashtag.test('#azs@'))
-//
-// const pristine = new Pristine(uploadForm);
-//
-// const checkHashtag = () => {
-//   return hashtag.test(hashTagInput.value);
-// }
-//
-// pristine.addValidator(hashTagInput, checkHashtag, 'неверный hashtag');
-//
-// uploadForm.addEventListener('submit', (evt) => {
-//   evt.preventDefault();
-//   pristine.validate();
-// });
+let defaultConfig = {
+  classTo: 'img-upload__form',
+  errorClass: 'img-upload__form--error',
+  successClass: 'img-upload__form--success',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__help'
+};
+
+const pristine = new Pristine(uploadForm, defaultConfig);
+
+const checkHashtag = (hashtagElement) => {
+  if (hashtag.test(hashtagElement)) {
+    return true;
+  }
+  return false;
+}
+
+const checkAllHashtags = () => {
+  const inputArray = hashTagInput.value.trim().split(' ');
+  return  inputArray.every(checkHashtag);
+}
+
+pristine.addValidator(hashTagInput, checkAllHashtags, 'Проверьте правильность хэштегов');
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
 
