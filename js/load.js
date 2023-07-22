c783d6ca5843bb4e417295772255a6ee6efba6ba
Vladislav Gernet <skeimorr@gmail.com>
2023-07-22 22:43:16 +0400

    data recieved!

--- js/load.js
+++ js/load.js
@@ -1,23 +1,23 @@
-const createLoader = () => () => fetch(
-  'https://29.javascript.pages.academy/kekstagram/data',
-  {
-    method: 'get',
-    credentials: 'same-origin',
-  },
-)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-
-    throw new Error(`${response.status} ${response.statusText}`);
-  })
-  .then((data) => {
-    console.log(data);
-    return data;
-  })
-  .catch((err) => {
-    alert(err);
-  });
-
-export {createLoader};
+// const createLoader = () => () => fetch(
+//   'https://29.javascript.pages.academy/kekstagram/data',
+//   {
+//     method: 'get',
+//     credentials: 'same-origin',
+//   },
+// )
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//
+//     throw new Error(`${response.status} ${response.statusText}`);
+//   })
+//   .then((data) => {
+//     // render(data);
+//     console.log(render)
+//   })
+//   .catch((err) => {
+//     alert(err);
+//   });
+//
+// export {createLoader};
--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 import {renderPhotoArray} from './rendering.js';
 import {newArrayOfObjects} from './data.js';
 import './upload-image.js';
-import {createLoader} from './load.js';
+// import {createLoader} from "./load.js";
 
-const loadData = createLoader();
-loadData();
+// createLoader('123');
 
-renderPhotoArray(newArrayOfObjects);
+fetch(
+  'https://29.javascript.pages.academy/kekstagram/data',
+  {
+    method: 'get',
+    credentials: 'same-origin',
+  },
+)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status} ${response.statusText}`);
+  })
+  .then((data) => {
+    renderPhotoArray(data);
+  })
+  .catch((err) => {
+    alert(err);
+  });
+
+
 
 
