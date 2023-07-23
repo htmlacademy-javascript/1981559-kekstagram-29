de7ef36244672fa4c0f5951e8e5af4218b56e5f7
Vladislav Gernet <skeimorr@gmail.com>
2023-07-23 09:36:37 +0400

    function updated

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 import {renderPhotoArray} from './rendering.js';
 import {newArrayOfObjects} from './data.js';
+import {getData, sendData} from './load-data.js';
+import {showAlert} from './util.js';
 import './upload-image.js';
-import  {loadPictures} from './load-data.js';
 
-loadPictures();
-
-
+getData()
+  .then((pictures) => {
+    renderPhotoArray(pictures);
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
 
 
 
