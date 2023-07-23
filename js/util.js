23a1f999aeaa4b8cb8daf4b4c5136f13371aa134
Vladislav Gernet <skeimorr@gmail.com>
2023-07-23 09:39:05 +0400

    font updated

--- js/util.js
+++ js/util.js
@@ -8,7 +8,7 @@ const showAlert = (message) => {
   alertContainer.style.top = '0';
   alertContainer.style.right = '0';
   alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '30px';
+  alertContainer.style.fontSize = '16px';
   alertContainer.style.textAlign = 'center';
   alertContainer.style.backgroundColor = 'red';
 
