5a00a322676679c6d085b8a668f180ec20145d73
Vladislav Gernet <skeimorr@gmail.com>
2023-07-29 19:06:37 +0400

    some issue with src is done

--- js/full-photo-creator.js
+++ js/full-photo-creator.js
@@ -1,7 +1,7 @@
 import {DEFAULT_SHOWN_COMMENTS} from './constats.js';
 
 const fillFullPhotoCardData = (PictureData, pictureId, arrayOfObjects) => {
-  PictureData.image.src = `./photos/${Number(pictureId) + 1}.jpg`;
+  PictureData.image.src = `photos/${Number(pictureId) + 1}.jpg`;
   PictureData.likes.textContent = String(arrayOfObjects[pictureId].likes);
   PictureData.description.textContent = arrayOfObjects[pictureId].description;
   PictureData.commentsCounter.textContent = `${DEFAULT_SHOWN_COMMENTS} из ${String(arrayOfObjects[pictureId].comments.length)} комментариев`;
