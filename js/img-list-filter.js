debd0646c063e7d7a7cb12083e5078d52267b424
Vladislav Gernet <skeimorr@gmail.com>
2023-07-30 21:37:14 +0400

    sort by comments added

--- js/img-list-filter.js
+++ js/img-list-filter.js
@@ -4,33 +4,4 @@ const showFiltersButtons = () => {
   imgFilters.classList.remove('img-filters--inactive');
 };
 
-// const Default = {
-//   COAT_COLOR: 'rgb(101, 137, 164)',
-//   EYES_COLOR: 'black',
-// };
-//
-//
-// const getImageRank = (image, comments, likes) => {
-//   const comments = document.querySelector('[name="coat-color"]');
-//   const likes = document.querySelector('[name="eyes-color"]');
-//
-//   let rank = 0;
-//
-//   if (wizard.colorCoat === (coatColorInput.value || Default.COAT_COLOR)) {
-//     rank += 2;
-//   }
-//   if (wizard.colorEyes === (eyesColorInput.value || Default.EYES_COLOR)) {
-//     rank += 1;
-//   }
-//
-//   return rank;
-// };
-//
-// const compareWizards = (wizardA, wizardB) => {
-//   const rankA = getWizardRank(wizardA);
-//   const rankB = getWizardRank(wizardB);
-//
-//   return rankB - rankA;
-// };
-
 export {showFiltersButtons};
--- js/rendering.js
+++ js/rendering.js
@@ -7,15 +7,31 @@ const renderPhotoArray = (photoArray) => {
   const pictureContainer = document.querySelector('.pictures');
   const photosFragment = document.createDocumentFragment();
 
-  photoArray.forEach(({id, url, description, likes, comments}) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.dataset.pictureId = id;
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__img').alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    photosFragment.appendChild(pictureElement);
-  });
+  const getImageRank = ({comments}) => {
+    let rank = 0;
+    rank += comments.length;
+    return rank;
+  };
+
+  const compareImages = (imageA, imageB) => {
+    const rankA = getImageRank(imageA);
+    const rankB = getImageRank(imageB);
+
+    return rankB - rankA;
+  };
+
+  photoArray
+    .slice()
+    .sort(compareImages)
+    .forEach(({id, url, description, likes, comments}) => {
+      const pictureElement = pictureTemplate.cloneNode(true);
+      pictureElement.dataset.pictureId = id;
+      pictureElement.querySelector('.picture__img').src = url;
+      pictureElement.querySelector('.picture__img').alt = description;
+      pictureElement.querySelector('.picture__likes').textContent = likes;
+      pictureElement.querySelector('.picture__comments').textContent = comments.length;
+      photosFragment.appendChild(pictureElement);
+    });
 
   pictureContainer.appendChild(photosFragment);
 
