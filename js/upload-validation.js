2386ccb3a430c821ab433f2e379039dc5f3aa1b9
Vladislav Gernet <skeimorr@gmail.com>
2023-07-30 18:08:14 +0400

    new otions to validation

--- js/upload-validation.js
+++ js/upload-validation.js
@@ -7,12 +7,12 @@ const createValidation = (inputField, comment, pristineConstructor) => {
   const checkHashtag = (hashtagElement) => hashtag.test(hashtagElement);
 
   const checkMaxHashtags = () => {
-    const inputArray = inputField.value.trim().split(' ');
+    const inputArray = inputField.value.trim().split(' ').filter((tag) => tag.length !== 0);
     return inputArray.length < MAX_AVAILABLE_HASHTAGS + 1;
   };
 
   const checkAllHashtags = () => {
-    const inputArray = inputField.value.trim().split(' ');
+    const inputArray = inputField.value.trim().split(' ').filter((tag) => tag.length !== 0);
     if (inputArray.length <= 1 && inputArray[0] === '') {
       return true;
     }
@@ -20,14 +20,14 @@ const createValidation = (inputField, comment, pristineConstructor) => {
   };
 
   const isHashtagRepeat = () => {
-    const inputArray = inputField.value.toLowerCase().trim().split(' ');
+    const inputArray = inputField.value.toLowerCase().trim().split(' ').filter((tag) => tag.length !== 0);
     return checkRepeat(inputArray);
   };
 
   const checkCommentFieldLength = () => comment.value.length <= MAX_COMMENT_WORDS;
 
   pristineConstructor.addValidator(inputField, checkMaxHashtags, `Указано больше ${MAX_AVAILABLE_HASHTAGS} хэштэгов`, 3, true);
-  pristineConstructor.addValidator(inputField, checkAllHashtags, 'Хэштэги должны состоять из букв и/или цифр. Не боелее 20 символов', 2, true);
+  pristineConstructor.addValidator(inputField, checkAllHashtags, 'Хэштэги должны состоять из букв и/или цифр. Не более 20 символов', 2, true);
   pristineConstructor.addValidator(inputField, isHashtagRepeat, 'Хэштэги повторяются', 1, true);
   pristineConstructor.addValidator(comment, checkCommentFieldLength, `Разрешено не более ${MAX_COMMENT_WORDS} символов`);
 };
