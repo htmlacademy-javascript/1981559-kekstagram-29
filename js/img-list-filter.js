33bd8c354a59e536bda424431daa37084c25e27a
Vladislav Gernet <skeimorr@gmail.com>
2023-07-30 20:49:37 +0400

    show filters added

---
+++ js/img-list-filter.js
@@ -0,0 +1,6 @@
+const showFiltersButtons = () => {
+  const imgFilters = document.querySelector('.img-filters');
+  imgFilters.classList.remove('img-filters--inactive');
+};
+
+export {showFiltersButtons};
--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ import {getData} from './load-data.js';
 import {showAlert} from './util.js';
 import {renderPhotoArray} from './rendering.js';
 import {initUploadImageForm} from './upload-image.js';
+import {showFiltersButtons} from './img-list-filter.js';
 
 initUploadImageForm();
 getData()
   .then(renderPhotoArray)
+  .then(showFiltersButtons)
   .catch(
     (err) => {
       showAlert(err.message);
