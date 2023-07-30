da2fee4f54716ded517f640c873c3811fc77b00e
Vladislav Gernet <skeimorr@gmail.com>
2023-07-30 20:50:29 +0400

    a tiny refactor

--- js/img-list-filter.js
+++ js/img-list-filter.js
@@ -1,5 +1,6 @@
+const imgFilters = document.querySelector('.img-filters');
+
 const showFiltersButtons = () => {
-  const imgFilters = document.querySelector('.img-filters');
   imgFilters.classList.remove('img-filters--inactive');
 };
 
