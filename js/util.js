5a660e035e618b52f04e1b0420717c31de58985e
Vladislav Gernet <skeimorr@gmail.com>
2023-07-30 20:38:04 +0400

    debounce and throttle added

--- js/util.js
+++ js/util.js
@@ -41,4 +41,23 @@ const checkRepeat = (arr) => {
   return arr.length === uniqueArr.size;
 };
 
-export {isEscapeKey, disableEscHandling, checkRepeat, pluralize, showAlert};
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const throttle = (callback, delayBetweenFrames) => {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+};
+
+export {isEscapeKey, disableEscHandling, checkRepeat, pluralize, showAlert, debounce, throttle};
